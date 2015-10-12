7fefa50516785ef9b96a79fde4911a6f05ad5efc
Rob <Rob@192.168.2.5>
2015-10-12 02:19:37 -0600

    I think that I'm getting an error with read write due to those operations opening up threads. The file tries to read and write at the same time. Productivity way down, though, will fix in morning.

--- MLRosenbrockEstimation/src/nnBuilder/ReadWrite.java
+++ MLRosenbrockEstimation/src/nnBuilder/ReadWrite.java
@@ -15,20 +15,24 @@ public class ReadWrite {
     static PrintStream fout;
     static BufferedReader fin;
 
-    // For writing to a file
-    ReadWrite() {
-        String filename = "savedNets/nn_0.csv";
-        try {
-            fout = new PrintStream(new FileOutputStream(filename));
-        } catch (IOException fo) {
-            System.out.println(fo);
-        }
-    }
+//    // For writing to a file
+//    ReadWrite() {
+//        String filename = "savedNets/nn_0.csv";
+//        try {
+//            fout = new PrintStream(new FileOutputStream(filename));
+//        } catch (IOException fo) {
+//            System.out.println(fo);
+//        }
+//    }
 
     // Reads a file in
+    // in means read a file in
+    // out means read a file out
     ReadWrite(String filename) {
         try {
-            fin = new BufferedReader(new FileReader(filename));
+                fin = new BufferedReader(new FileReader(filename));
+            
+                fout = new PrintStream(new FileOutputStream(filename));
 
         } catch (IOException fo) {
             System.out.println(fo);
@@ -45,6 +49,13 @@ public class ReadWrite {
 
         fout.println(out);
 
+    }
+    
+    private static void writerNoPrintln(String s) {
+        // DELETE
+        System.out.printf("%s", s);
+        // END DELETE
+        fout.println(s);
     }
 
     public void writer(char out) {
@@ -104,53 +115,52 @@ public class ReadWrite {
         writer(sb.toString());
     }
 
-    public static ArrayList<NNLayer> readNeuralNet() {
-        ArrayList<NNLayer> nNet = new ArrayList<NNLayer>();
-        File saveDirectory = new File("savedNets");
-        String[] fileNames = saveDirectory.list();
-        System.out.println();
-        // List all of the files in the save directory
-        for (int i = 0; i < fileNames.length; i++) {
-            System.out.printf("%d. %s%n", i + 1, fileNames[i]);
-        }
-        // Get user input for which net to use
-//        System.out.printf("Which neural net would you like to use? >");
-//        Scanner in = new Scanner(System.in);
-//        in.next();
-//        int choice = in.nextInt() - 1;
-//        // Create instance of this class to read the specified file
-        ReadWrite r = new ReadWrite("savedNets/nn_0.csv");
-        String curLine;
-        NNLayer curLayer = new NNLayer(0);
-        try {
-            while ((curLine = fin.readLine()) != null) {
-                // If the current line is "num_nodes," and one or more digits...
-                if (curLine.matches("num_nodes,\\d+")) {
-                    // Add the new layer to the neural net
-                    curLayer = new NNLayer(parseNumberOfInputs(curLine));
-                    // Initialize nodes for layer as they are not randomly
-                    // populated
-                    curLayer.initializeNodes();
-                    nNet.add(curLayer);
-                }
-                // If the line matches 0 or 1 negative signs, a digit, 0 or 1
-                // periods, 0 or more digits, and 0 or one commas, multiple
-                // times...
-                else if (curLine.matches("(-?\\d.?\\d*,?)+")) {
-                    curLayer.sequentialNodeUpdate().updateWeights(parseNumberOfWeights(curLine)); 
-                }
-            }
-        } catch (IOException e) {
-            System.out.printf("%n%nError, could not read from file");
-            System.exit(1);
-        }
-        return nNet;
-    }
-    
+//    public static ArrayList<NNLayer> readNeuralNet() {
+//        ArrayList<NNLayer> nNet = new ArrayList<NNLayer>();
+//        File saveDirectory = new File("savedNets");
+//        String[] fileNames = saveDirectory.list();
+//        System.out.println();
+//        // List all of the files in the save directory
+//        for (int i = 0; i < fileNames.length; i++) {
+//            System.out.printf("%d. %s%n", i + 1, fileNames[i]);
+//        }
+//        // Get user input for which net to use
+////        System.out.printf("Which neural net would you like to use? >");
+////        Scanner in = new Scanner(System.in);
+////        in.next();
+////        int choice = in.nextInt() - 1;
+////        // Create instance of this class to read the specified file
+//        ReadWrite r = new ReadWrite("savedNets/nn_0.csv");
+//        String curLine;
+//        NNLayer curLayer = new NNLayer(0);
+//        try {
+//            while ((curLine = fin.readLine()) != null) {
+//                // If the current line is "num_nodes," and one or more digits...
+//                if (curLine.matches("num_nodes,\\d+")) {
+//                    // Add the new layer to the neural net
+//                    curLayer = new NNLayer(parseNumberOfInputs(curLine));
+//                    // Initialize nodes for layer as they are not randomly
+//                    // populated
+//                    curLayer.initializeNodes();
+//                    nNet.add(curLayer);
+//                } // If the line matches 0 or 1 negative signs, a digit, 0 or 1
+//                // periods, 0 or more digits, and 0 or one commas, multiple
+//                // times...
+//                else if (curLine.matches("(-?\\d.?\\d*,?)+")) {
+//                    curLayer.sequentialNodeUpdate().updateWeights(parseNumberOfWeights(curLine));
+//                }
+//            }
+//        } catch (IOException e) {
+//            System.out.printf("%n%nError, could not read from file");
+//            System.exit(1);
+//        }
+//        return nNet;
+//    }
+
     /**
-     * 
+     *
      * @param s
-     * @return 
+     * @return
      */
     private static int parseNumberOfInputs(String s) {
         Pattern p = Pattern.compile("-?\\d+");
@@ -159,11 +169,11 @@ public class ReadWrite {
         m.group();
         return Integer.parseInt(m.group());
     }
-    
+
     /**
-     * 
+     *
      * @param s
-     * @return 
+     * @return
      */
     private static double[] parseNumberOfWeights(String s) {
         String[] weightsAsStrings = s.split(",");
@@ -172,17 +182,61 @@ public class ReadWrite {
             weightsAsDoubles[i] = Double.parseDouble(weightsAsStrings[i]);
         }
         return weightsAsDoubles;
-    }   
+    }
 
     /**
-     *
+     * Generates a CSV with Time,# of Layers, and the learning rate
      */
-//   private static NNLayer createLayer() {
-//       NNLayer n;
-//       
-//   }
-//   
-//   private static Node translateLine() {
-//       
-//   }
+    public static void outputCSVInfo(String s) {
+        StringBuffer data = new StringBuffer(readCSV());
+        //System.out.printf("%n%s%n", data.toString());
+        data.append(s);
+        data.append(String.format("%n"));
+        updateFileOut("netData/networkTrainingData.csv");
+        writerNoPrintln(data.toString());
+    }
+
+    private static String readCSV() {
+        updateFileIn("netData/networkTrainingData.csv");
+        StringBuilder s = new StringBuilder();
+        String curLine;
+        try {
+            while ((curLine = fin.readLine()) != null) {
+                s.append(curLine);
+                System.out.printf("%s%n", curLine);
+            }
+        } catch (IOException e) {
+            System.out.printf("%n%nError, could not read from file %s");
+            System.exit(1);
+        }
+        try {
+        fin.close();
+        } catch (IOException e) {
+            System.out.println("Could not close non opened file reader");
+        }
+        return s.toString();
+    }
+    
+    private static void updateFileIn(String filename) {
+        try {
+                fout.close();
+                fin = new BufferedReader(new FileReader(filename));
+        } catch (IOException fo) {
+            System.out.println(fin);
+        } catch (NullPointerException np) {
+            // Do nothing.
+        }
+    }
+    
+    private static void updateFileOut(String filename) {
+         try {
+                fin.close();
+                fout = new PrintStream(new FileOutputStream(filename));
+
+        } catch (IOException fo) {
+            System.out.println(fo);
+        } catch (NullPointerException np) {
+            // Do nothing.
+        }
+    }
 }
